43d9ede8a91c8ea49c085f183af00baa1ae8dd46
DeepralShakya <shakyadeepral44@gmail.com>
2024-02-15 14:31:13 +0545

    Some adjustments

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -32,12 +32,12 @@ const Cart = () =>{
     return (
     <Sheet>
         <SheetTrigger className="group -m-2 flex items-center p-2">
-            <ShoppingCart aria-hidden="true" className="h-6 w-6 flex-shrink-0 text-orange-400 group-hover:text-orange-600"></ShoppingCart>
+            <ShoppingCart aria-hidden="true" className="h-6 w-6 flex-shrink-0 text-orange-400 group-hover:text-orange-600"/>
             <span className="ml-2 text-sm font-medium text-black group-hover:text-orange-800">{isMounted ? itemCount: 0}</span>
         </SheetTrigger>
         <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
             <SheetHeader className="space-y-3 pr-6">
-                <SheetTitle>Cart({itemCount})</SheetTitle>
+                <SheetTitle>Cart ({itemCount})</SheetTitle>
             </SheetHeader>
             {itemCount > 0 ? (
                 <>
@@ -66,7 +66,7 @@ const Cart = () =>{
                         </div>
 
                         <SheetFooter>
-                            <SheetTrigger className="w-full">
+                            <SheetTrigger asChild>
                                 <Link href='/cart' className={buttonVariants({
                                     className: "w-full",
                                 })}>Checkout</Link>
@@ -74,23 +74,24 @@ const Cart = () =>{
                         </SheetFooter>
                     </div>
                 </>
-            ) : (<div className="flex h-full flex-col items-center justify-center space-y-1">
-                <div aria-hidden="true" className="relative mb-4 h-60 w-60 text-muted-foreground">
-                    <Image src='/images/empty_cart.jpg' fill alt="Empty Shopping Card"/>
-                </div>
-                <div className="text-xl font-bold">
-                    Empty Cart
-                </div>
-                <SheetTrigger className="w-full">
-                    <Link href='/products' className={buttonVariants({
-                        variant: "link",
-                        size: "sm",
-                        className: "text-sm text-muted-foreground"
-                    })}>
-                        Add items to your cart
-                    </Link>
-                </SheetTrigger>
-            </div>)}
+            ) : (
+                <div className="flex h-full flex-col items-center justify-center space-y-1">
+                    <div aria-hidden="true" className="relative mb-4 h-60 w-60 text-muted-foreground">
+                        <Image src='/images/empty_cart.jpg' fill alt="Empty Shopping Card"/>
+                    </div>
+                    <div className="text-xl font-bold">
+                        Empty Cart
+                    </div>
+                    <SheetTrigger className="w-full">
+                        <Link href='/products' className={buttonVariants({
+                            variant: "link",
+                            size: "sm",
+                            className: "text-sm text-muted-foreground"
+                        })}>
+                            Add items to your cart
+                        </Link>
+                    </SheetTrigger>
+                </div>)}
         </SheetContent>
     </Sheet>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,12 +31,12 @@ const Navbar = async () => {
                             <div className="ml-auto flex items-center">
                                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                                     {user ? null: (
-                                    <Link href='sign-in' className={buttonVariants({variant: "ghost"})}>Sign in</Link>
+                                    <Link href='/sign-in' className={buttonVariants({variant: "ghost"})}>Sign in</Link>
                                     )}
 
                                     {user ? null: <span className="h-6 w-px bg-orange-300" aria-hidden="true"/>}
 
-                                    {user ? <UserAccountNav user={user} /> : (<Link href="/sign-up" className={buttonVariants({variant: "ghost"})}>Create Account</Link>)}
+                                    {user ? <UserAccountNav user={user} /> : <Link href="/sign-up" className={buttonVariants({variant: "ghost"})}>Create Account</Link>}
 
                                     {user ? <span className="h-6 w-px bg-orange-300" aria-hidden="true"/> : null}
 
